5714ee85cc5cf091b841b7a921c56482741eef27
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2023-10-24 14:24:47 +0200

    working on details and header
    working on details and header

--- meetups-front-end/src/views/MeetupDetailView/MeetupDetailView.tsx
+++ meetups-front-end/src/views/MeetupDetailView/MeetupDetailView.tsx
@@ -1,5 +1,31 @@
 import "./MeetupDetailView.scss";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
 
 export default function MeetupDetailView() {
-  return <div className="view"></div>;
+
+  function clickRegister() { }
+  function handleClick() { }
+  const title = "Hundar utan öron";
+  const date = "2021-09-01";
+  const venue = "Hundparken";
+  const info = "Vi träffas och pratar om hundar utan öron lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  const host = "Brukshundsklbben";
+  const numberOfAttendees = 5;
+  const maxNumberOfAttendees = 10;
+  return <div className="view meetupDetail-view">
+    <Header showHomeBtn={true} showMyPageBtn={true} onClick={handleClick} />
+    <section className="detail-container">
+      <h1>{title}</h1>
+      <h2>Datum: {date}</h2>
+      <h2>Plats: {venue}</h2>
+      <h2>Info: {info}</h2>
+      <h2>Arrangör: {host}</h2>
+      <h2>Anmälda: {numberOfAttendees}/{maxNumberOfAttendees}</h2>
+    </section>
+    <Footer buttonText="Anmäl mig!" type="detail-unannounced-view" onClick={clickRegister} />
+
+
+  </div>;
 }
