11606dab7bfb11f1b4f17d6f898e0a382e6bcc96
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2023-10-26 10:31:02 +0200

    Update ProfileView.tsx

--- meetups-front-end/src/views/ProfileView/ProfileView.tsx
+++ meetups-front-end/src/views/ProfileView/ProfileView.tsx
@@ -1,5 +1,18 @@
 import "./ProfileView.scss";
+import Header from "../../components/Header/Header";
+
+
+
 
 export default function ProfileView() {
-  return <></>;
+  function handleClick() { }
+
+  const userName = "Kalle";
+
+  return <div className="view profile-view">
+    <Header showHomeBtn={true} showMyPageBtn={false} onClick={handleClick} />
+    <section className="profile-container"></section>
+    <h1>{userName}s meetups</h1>
+
+  </div>;
 }
