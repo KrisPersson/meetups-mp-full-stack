8423b4e9cb77133f3d2e9a157e4bb8837236cf59
ErsHoeghet <112584470+ErsHoeghet@users.noreply.github.com>
2023-10-27 09:26:58 +0200

    Ny BASE_URL

--- meetups-front-end/src/api/index.ts
+++ meetups-front-end/src/api/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://5zpz11tlxd.execute-api.eu-north-1.amazonaws.com/api'
+export const BASE_URL = 'https://r45j1d490b.execute-api.eu-north-1.amazonaws.com/api'
 export * from './meetups'
 export * from './user'
