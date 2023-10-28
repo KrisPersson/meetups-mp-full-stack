1a32c722cd646f7bc881f9239040b2bcfcd9096f
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2023-10-28 02:08:00 +0200

    search

--- meetups-front-end/src/views/MeetupsView/MeetupsView.tsx
+++ meetups-front-end/src/views/MeetupsView/MeetupsView.tsx
@@ -1,65 +1,89 @@
-import './MeetupsView.scss';
-import Header from '../../components/Header/Header';
-import Input from '../../components/Input/Input';
-import { apiGetUpcomingMeetUps } from '../../api/meetups';
+import "./MeetupsView.scss";
+import Header from "../../components/Header/Header";
+import Input from "../../components/Input/Input";
+import { apiGetUpcomingMeetUps } from "../../api/meetups";
 // import meetupArr from "../../components/Meetups/Meetups";
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { truncate } from '../../utils/index';
-import { MeetupFromDb } from '../../types/index';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { MeetupFromDb } from "../../types/index";
+import { MeetupListItem } from "../../components/MeetupListItem/MeetupListItem";
 
 export default function MeetupsView() {
   const navigate = useNavigate();
   const [meetupItems, setMeetupItems] = useState<MeetupFromDb[]>([]);
+  const [searchedItems, setSearchedItems] = useState<MeetupFromDb[]>([]);
+  const [searchInput, setSearchInput] = useState<string>("");
 
   useEffect(() => {
     fetchMeetups();
   }, []);
 
   async function fetchMeetups() {
-    const token = localStorage.getItem('userToken') || '';
+    const token = localStorage.getItem("userToken") || "";
     const dataFromDb = await apiGetUpcomingMeetUps(token);
     setMeetupItems([...dataFromDb.meetups]);
   }
 
-  const renderedMeetupItems =
-    meetupItems.length > 0
-      ? meetupItems.map((meetup) => (
-          <section
-            key={meetup.PK}
-            className='meetups'
-            onClick={() => seeMeetup(meetup)()}
-          >
-            <h2 className='meetups__meetupsTitle'>{meetup.Title}</h2>
-            <p>
-              {meetup.StartTime} | {meetup.Location} |{' '}
-              {truncate(meetup.Description, 40)} | {meetup.Host}
-            </p>
-          </section>
-        ))
-      : [];
-
   function seeMeetup(meetup: MeetupFromDb) {
     return function () {
-      navigate('/detail/', { state: { meetup: meetup } });
+      navigate("/detail/", { state: { meetup: meetup } });
     };
   }
 
   function handleClick() {
-    console.log('test');
+    console.log("test");
   }
 
+  function handleSearchInputChange(value: string) {
+    setSearchInput(value);
+    searchMeetups(value);
+  }
+
+  function searchMeetups(value: string) {
+    const filtered = meetupItems.filter((meetup) => {
+      const desc = meetup.Description.toLowerCase();
+      const title = meetup.Title.toLowerCase();
+      return (
+        desc.includes(value.toLowerCase()) ||
+        title.includes(value.toLowerCase())
+      );
+    });
+    setSearchedItems([...filtered]);
+  }
+
+  const renderedMeetupItems = () => {
+    const toBeRendered =
+      searchInput.length > 0
+        ? [...searchedItems]
+        : meetupItems.length > 0
+        ? [...meetupItems]
+        : [];
+    return toBeRendered.map((meetup) => (
+      <MeetupListItem
+        key={meetup.PK}
+        {...meetup}
+        seeMeetup={seeMeetup(meetup)}
+      />
+    ));
+  };
+
   return (
-    <div className='view meetups-view'>
+    <div className="view meetups-view">
       <Header showHomeBtn={false} showMyPageBtn={true} onClick={handleClick} />
-      <section className='meetups-container'>
+      <section className="meetups-container">
         <h1>MeetApp</h1>
-        <div className='meetups-container__search-container'>
-          <Input label='Sök event' htmlFor='search' />
-          <button onClick={handleClick}>Hitta event till mig </button>
+        <div className="meetups-container__search-container">
+          {/* <Input label="Sök event" htmlFor="search" /> */}
+          <input
+            type="text"
+            defaultValue={searchInput}
+            placeholder="Search Meetups..."
+            onChange={(e) => handleSearchInputChange(e.target?.value)}
+          />
+          {/* <button onClick={handleClick}>Hitta event till mig </button> */}
         </div>
-        <section className='show-all-upcoming-meetups-container'>
-          {renderedMeetupItems}
+        <section className="show-all-upcoming-meetups-container">
+          {renderedMeetupItems()}
         </section>
       </section>
     </div>
