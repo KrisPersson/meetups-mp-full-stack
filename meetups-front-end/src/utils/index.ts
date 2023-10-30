db8aa4a6a6fd12c9fdfe2460b1e680d70df13553
William Hoang Dinh <hoangdinh2411@gmail.com>
2023-10-30 12:59:36 +0100

    update APP URL env

--- meetups-front-end/src/utils/index.ts
+++ meetups-front-end/src/utils/index.ts
@@ -2,4 +2,4 @@ export function truncate(str: string, n: number) {
   return str.length > n ? str.slice(0, n - 1) + '...' : str;
 }
 
-export const APP_URL = import.meta.env.VITE_APP_URL as string;
+export const APP_URL = (import.meta.env.VITE_APP_URL as string) || '/';
