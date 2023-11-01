9699eb752378924af2f521351b50ba5193fbe6e5
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2023-11-01 11:27:14 +0100

    fel-fix

--- meetups-front-end/src/components/MeetupListItem/MeetupListItem.tsx
+++ meetups-front-end/src/components/MeetupListItem/MeetupListItem.tsx
@@ -7,11 +7,36 @@ type MeetupFromDbPlusSeeMeetup = MeetupFromDb & {
 };
 
 export function MeetupListItem(props: MeetupFromDbPlusSeeMeetup) {
-  const { PK, Title, StartTime, Location, Host, Description, seeMeetup } =
-    props;
+  const {
+    PK,
+    Title,
+    StartTime,
+    Location,
+    Host,
+    Description,
+    MaxAttendants,
+    CurrentAttendants,
+    seeMeetup,
+  } = props;
 
   return (
-    <section key={PK} className="meetups" onClick={seeMeetup}>
+    <section
+      key={PK}
+      className="meetups"
+      onClick={() =>
+        seeMeetup({
+          PK,
+          Title,
+          StartTime,
+          Location,
+          Host,
+          Description,
+          MaxAttendants,
+          CurrentAttendants,
+          SK: props.SK,
+        })
+      }
+    >
       <h2 className="meetups__meetupsTitle">{Title}</h2>
       <p>
         {StartTime} | {Location} | {truncate(Description, 40)} | {Host}
